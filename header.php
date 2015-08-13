ee8b972231ea932f3cb8d6c217c30a55ac0412d6
S Raghav <ragsvds710@gmail.com>
2015-08-13 22:53:08 +0530

    Modify header to use hamburger logo

--- header.php
+++ header.php
@@ -5,25 +5,37 @@
 <script src="js/jquery-2.1.3.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
 <body>
-<nav class="navbar navbar-inverse">
+<!-- We need to collapse the navbar for small screens-->
+<nav class="navbar navbar-inverse" role="navigation">
+<div class="navbar-header">
+<button type="button" class="navbar-toggle" data-toggle="collapse"
+         data-target="#reachoutNavbar">
+         <span class="sr-only">Toggle navigation</span>
+        <span  class="icon-bar"></span>
+        <span  class="icon-bar"></span>
+        <span  class="icon-bar success"></span>
+</button>
 <a class="navbar-brand">Reach Out</a>
+</div>
+<div class="collapse navbar-collapse navbar-right" id="reachoutNavbar">
 <?php
 if (isset($_SESSION['id'])){
 ?>
-<ul class="nav navbar-nav pull-right">
+<ul class="nav navbar-nav text-center">
 <li>
 <a href="jobfeed.php" ><strong style="color:white">Job Feed</strong></a></li>
 <li>
-<a href="#">Hi,<?php echo $_SESSION['id'] ?> <span class="glyphicon glyphicon-user"></span>
+<a href="profile.php">Hi,<?php echo $_SESSION['id'] ?> <span class="glyphicon glyphicon-user"></span>
 </a>
 </li>
 <li>
 <a class="navbar-nav">
-<span class="glyphicon glyphicon-hand-right" style="color:yellow"> <?php echo $_SESSION['reliability'] ?></span>
+<span class="glyphicon glyphicon-thumbs-up" style="color:yellow"> <?php echo $_SESSION['reliability'] ?></span>
 </a>
 </li>
 <li>
 <a class="navbar-nav" href="logout.php" data-toggle="tooltip" title="signout">
+<strong style="color:white;">Logout</strong>
 <span class="glyphicon glyphicon-log-out " style="color:white;"></span> </a>
 </li>
 </ul>
@@ -40,5 +52,6 @@ else { ?>
 </div>
 <button class="btn btn-primary ">Let's Go</button>
 </form>
+</div>
 <?php } ?>
 </nav>
